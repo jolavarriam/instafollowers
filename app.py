fc4e5ff32644ec8ea93e5708ef281cd395ca30d0
Jonathan <jonathan.olavarria@usm.cl>
2020-11-10 21:24:49 -0300

    return 200 privacy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
@@ -6,48 +6,40 @@ app = Flask(__name__)
 def safe_string(string):
     return ''.join([c for c in string if c.isalnum() or c in ['.','-','_']])
 
-@app.route('/privacy')
-def site_privacy():
-    return '<html><body>Política de privacidad</body></html>'
-
 @app.route('/api/followers/<username>')
 def followers(username):
     username = safe_string(username)
-    r = requests.get('https://instagram.com/'+username+'?__a=1')
-    text_file = open("requests.txt", "wb")
-    text_file.write(r.content)
-    text_file.close()
-    #print(r)
-    #token = '"userInteractionCount":"'
-    #start = r.text.find(token) 
-    #start += len(token)
-    #end = r.text.find('"}',start)
-    #followers = r.text[start:end]
-    path = "requests.txt"
-    return send_file(path, as_attachment=True)
-    #return followers
+    r = requests.get('https://instagram.com/'+username) #+'?__a=1'
+    token = '"userInteractionCount":"'
+    start = r.text.find(token) 
+    start += len(token)
+    end = r.text.find('"}',start)
+    followers = r.text[start:end]
+    return followers
 
 @app.route('/api/picture/<username>')
 def picture(username):
     username = safe_string(username)
-    r = requests.get('https://instagram.com/'+username+'?__a=1')
+    r = requests.get('https://instagram.com/'+username)
     token = 'profile_pic_url":"'
     start = r.text.find(token) 
     start += len(token)
     end = r.text.find('",', start)
     profile_url = r.text[start:end].replace("\\u0026", "&")
-    #print(profile_url)
     return profile_url
 
 @app.route('/<username>')
 def index(username):
     username = safe_string(username)
-    return followers(username) 
-    #return render_template('index.html', username=username, followers=followers(username), picture=picture(username))
+    return render_template('index.html', username=username, followers=followers(username), picture=picture(username))
 
 @app.route('/')
 def default_index():
     return index("j.olavarria.m")
 
+@app.route('/privacy')
+def site_privacy():
+    return 200
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, port=80)
